549c6c5837512911794730d1b37338192723038c
Roman Peshkov <peshkovroman@gmail.com>
2018-08-04 21:40:04 +0200

    Initial commit

---
+++ src/extension.ts
@@ -0,0 +1,17 @@
+'use strict';
+
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+
+    console.log('Congratulations, your extension "vscode-di" is now active!');
+
+    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
+        vscode.window.showInformationMessage('Hello World!');
+    });
+
+    context.subscriptions.push(disposable);
+}
+
+export function deactivate() {
+}
